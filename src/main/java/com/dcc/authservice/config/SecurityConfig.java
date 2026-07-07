a6afdc5cd4ea7abefd9985ea2083c57936a59136
srun.veng <srun.veng@wingbank.com.kh>
2026-07-07 12:09:56 +0700

    auth-validation

--- src/main/java/com/dcc/authservice/config/SecurityConfig.java
+++ src/main/java/com/dcc/authservice/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/auth/login",
+                                "/auth/**",
                                 "/.well-known/jwks.json",
                                 "/actuator/health")
                         .permitAll()
